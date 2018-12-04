dc9782aec3474ad863aaba35e4874557f379f8de
Aaron Schlesinger <arschles@protonmail.com>
2018-12-04 12:54:59 -0800

    adding ACI

--- aci.sh
+++ aci.sh
@@ -2,15 +2,16 @@
 
 set -eou pipefail
 
-az container create \
--g "${AZURE_ATHENS_RESOURCE_GROUP}" \
--n "${AZURE_ATHENS_CONTAINER_NAME}" \
---image gomods/athens:v0.2.0 \
--e "ATHENS_STORAGE_TYPE=mongo" "ATHENS_MONGO_STORAGE_URL=${AZURE_ATHENS_MONGO_URL}" \
---ip-address=Public \
---dns-name="${AZURE_ATHENS_DNS_NAME}" \
---ports="3000"
-
-
-az container show -n "${AZURE_ATHENS_CONTAINER_NAME}" -g "${AZURE_ATHENS_RESOURCE_GROUP}"
-az container logs -n "${AZURE_ATHENS_CONTAINER_NAME}" -g "${AZURE_ATHENS_RESOURCE_GROUP}"
+for LOCATION in "eastus2" "westeurope" "southeastasia"
+do
+    echo "Deploying in ${LOCATION}"
+    az container create \
+    -g "${AZURE_ATHENS_RESOURCE_GROUP}" \
+    -n "${AZURE_ATHENS_CONTAINER_NAME}-${LOCATION}" \
+    --image gomods/athens:v0.2.0 \
+    -e "ATHENS_STORAGE_TYPE=mongo" "ATHENS_MONGO_STORAGE_URL=${AZURE_ATHENS_MONGO_URL}" \
+    --ip-address=Public \
+    --dns-name="${AZURE_ATHENS_DNS_NAME}-${LOCATION}" \
+    --ports="3000" \
+    --location=${LOCATION}
+done
