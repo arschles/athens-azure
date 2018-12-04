be449ead10a60635ba200726fe640c177b759ee8
Aaron Schlesinger <arschles@protonmail.com>
2018-12-04 12:21:53 -0800

    more to the aci script

--- aci.sh
+++ aci.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-set -eoux pipefail
+set -eou pipefail
 
 az container create \
 -g "${AZURE_ATHENS_RESOURCE_GROUP}" \
 -n "${AZURE_ATHENS_CONTAINER_NAME}" \
 --image gomods/athens:v0.2.0 \
--e "ATHENS_STORAGE_TYPE=mongo ATHENS_MONGO_STORAGE_URL=${AZURE_ATHENS_MONGO_URL}" \
+-e "ATHENS_STORAGE_TYPE=mongo" "ATHENS_MONGO_STORAGE_URL=${AZURE_ATHENS_MONGO_URL}" \
 --ip-address=Public \
---dns-name="${AZURE_ATHENS_DNS_NAME}"
+--dns-name="${AZURE_ATHENS_DNS_NAME}" \
+--ports="3000"
 
 
+az container show -n "${AZURE_ATHENS_CONTAINER_NAME}" -g "${AZURE_ATHENS_RESOURCE_GROUP}"
+az container logs -n "${AZURE_ATHENS_CONTAINER_NAME}" -g "${AZURE_ATHENS_RESOURCE_GROUP}"
