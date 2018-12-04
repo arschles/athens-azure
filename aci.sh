074074f674dd94938a93a7d81f31a15ce0e76607
Aaron Schlesinger <arschles@protonmail.com>
2018-12-04 12:07:07 -0800

    initial aci deploy script

---
+++ aci.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eoux pipefail
+
+az container create \
+-g "${AZURE_ATHENS_RESOURCE_GROUP}" \
+-n "${AZURE_ATHENS_CONTAINER_NAME}" \
+--image gomods/athens:v0.2.0 \
+-e "ATHENS_STORAGE_TYPE=mongo ATHENS_MONGO_STORAGE_URL=${AZURE_ATHENS_MONGO_URL}" \
+--ip-address=Public \
+--dns-name="${AZURE_ATHENS_DNS_NAME}"
+
+
