c3da910d33648db96d281f7e14cd4f291a2e63d9
Aaron Schlesinger <aaron@ecomaz.net>
2019-04-17 15:31:53 -0700

    Adding to the env checker

--- pkg/env/checker.go
+++ pkg/env/checker.go
@@ -5,10 +5,24 @@ import (
 	"os"
 )
 
+type Defaulter func(string) (string, error)
+
 func Check(key string) (string, error) {
 	val := os.Getenv(key)
 	if val == "" {
 		return "", fmt.Errorf("Missing environment variable %s", key)
 	}
 	return val, nil
+}
+
+func CheckOr(key string, fn Defaulter) (string, error) {
+	ret, err := Check(key)
+	if err == nil {
+		return ret, nil
+	}
+	ret, err = fn(key)
+	if err != nil {
+		return "", err
+	}
+	return ret, nil
 }
