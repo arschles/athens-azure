9da0a04e672434c348e03ca1c5bd35232c8a329a
Aaron Schlesinger <aaron@ecomaz.net>
2019-04-18 14:22:06 -0700

    adding some godoc

--- pkg/kube/ingress.go
+++ pkg/kube/ingress.go
@@ -2,14 +2,20 @@ package kube
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ericchiang/k8s"
+	corev1 "github.com/ericchiang/k8s/apis/core/v1"
 	extv1beta1 "github.com/ericchiang/k8s/apis/extensions/v1beta1"
 )
 
+// Ingress is a convenience wrapper around the Kubernetes Ingress resource
 type Ingress struct {
-	Installer
+	fmt.Stringer
 	core *extv1beta1.Ingress
+	// Note: Ingresses implement Resource, but don't embed it here
+	// because the compiler will not give errors if you don't implement
+	// a method in Resource when you put it into a profile
 }
 
 // IngressFromCore creates a new ingress from a core ingress value
