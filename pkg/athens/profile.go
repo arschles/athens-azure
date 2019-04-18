92d4c5690295d2ef251d146f82e6c598d654e28e
Aaron Schlesinger <aaron@ecomaz.net>
2019-04-18 09:18:46 -0700

    adding an athensProfile function

---
+++ pkg/athens/profile.go
@@ -0,0 +1,19 @@
+package athens
+
+import "github.com/arschles/athens-azure/pkg/kube"
+
+func newProfile(img string) *kube.Profile {
+	depl := athensDeployment(img)
+	return kube.NewManualProfile([]kube.Resource{depl})
+}
+
+func athensDeployment(img string) *kube.Deployment {
+	clist := containerList(img)
+	return kube.NewDeployment(name, namespace, containerList)
+}
+
+func containerList(img string) kube.ContainerList {
+	return kube.ContainerList{
+		kube.NewContainer(name, img),
+	}
+}
