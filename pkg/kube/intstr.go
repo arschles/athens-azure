a10e0da36cd98f16308637f35fa3814b371c194e
Aaron Schlesinger <aaron@ecomaz.net>
2019-04-18 11:25:58 -0700

    adding utility to create a new IntOrString
    cmon wtf is this type

---
+++ pkg/kube/intstr.go
@@ -0,0 +1,12 @@
+package kube
+
+import (
+	"github.com/ericchiang/k8s"
+	intstr "github.com/ericchiang/k8s/util/intstr"
+)
+
+func newIntOrString(i int32) *intstr.IntOrString {
+	return &intstr.IntOrString{
+		IntVal: k8s.Int32(i),
+	}
+}
