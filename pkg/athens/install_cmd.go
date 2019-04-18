0b4ec29f3056f6b963a4313f69da2a573e928ed9
Aaron Schlesinger <aaron@ecomaz.net>
2019-04-18 14:42:45 -0700

    removing commented code

--- pkg/athens/install_cmd.go
+++ pkg/athens/install_cmd.go
@@ -40,33 +40,6 @@ func installCmd(ctx cmd.Context) *cobra.Command {
 			return err
 		}
 		ctx.Infof("Done")
-		// if err := kube.UpsertNamespace(ctx, cl, namespace); err != nil {
-		// 	return errors.WithStack(err)
-		// }
-		// ctx.Debugf("Created namespace %s", namespace)
-		// ctx.Debugf("Creating service")
-		// service := athensService(port)
-		// if err := service.Install(ctx, cl); err != nil {
-		// 	return err
-		// }
-
-		// ctx.Debugf("Deploying %s", img)
-		// deployment := athensDeployment(img)
-		// if err := deployment.Install(ctx, cl); err != nil {
-		// 	return err
-		// }
-
-		// return helm.Install("./charts/athens", "athens", "athens", []helm.Set{
-		// 	{Name: "goGetWorkers", Val: "2"},
-		// })
-		// ctx.Debugf("Deployment created")
-
-		// ctx.Debugf("Creating ingress")
-		// ingress :=  athensIngress(svc)
-		// if err := ingress.Install(ctx, cl); err != nil {
-		// 	return err
-		// }
-		// ctx.Debugf("Ingress created")
 		return nil
 	}
 	return ret
