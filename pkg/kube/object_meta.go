29c93af390b7242942d28ad07e2a582553782b0c
Aaron Schlesinger <aaron@ecomaz.net>
2019-04-18 14:22:42 -0700

    adding help for objectMeta

--- pkg/kube/object_meta.go
+++ pkg/kube/object_meta.go
@@ -9,6 +9,15 @@ func objectMeta(name, ns string) *metav1.ObjectMeta {
 	return &metav1.ObjectMeta{
 		Name:      k8s.String(name),
 		Namespace: k8s.String(ns),
-		Labels:    make(map[string]string),
 	}
+}
+
+func objectMetaWithLabels(
+	name,
+	ns string,
+	labels map[string]string,
+) *metav1.ObjectMeta {
+	ret := objectMeta(name, ns)
+	ret.Labels = labels
+	return ret
 }
