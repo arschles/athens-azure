754406d1a0a4a02622ce264eba911be0e012cd57
Aaron Schlesinger <aaron@ecomaz.net>
2019-04-18 14:35:22 -0700

    adding an uninstall command

--- pkg/athens/root.go
+++ pkg/athens/root.go
@@ -11,6 +11,7 @@ const name = "athens"
 func Root(ctx cmd.Context) *cobra.Command {
 	ret := cmd.Skeleton("athens", "Install, update, and administrate Athens")
 	ret.AddCommand(installCmd(ctx))
+	ret.AddCommand(uninstallCmd(ctx))
 	ret.AddCommand(upgradeCmd(ctx))
 	ret.AddCommand(currentCmd(ctx))
 	return ret
---
+++ pkg/athens/uninstall_cmd.go
@@ -0,0 +1,40 @@
+package athens
+
+import (
+	"github.com/arschles/athens-azure/pkg/cmd"
+	"github.com/arschles/athens-azure/pkg/kube"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+func uninstallCmd(ctx cmd.Context) *cobra.Command {
+	ret := cmd.Skeleton("uninstall", "Uninstall Athens")
+	ret.RunE = func(cmd *cobra.Command, args []string) error {
+		cl, err := kube.LoadClientFromDiskKubeConfig()
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		athensProfile := newProfile("")
+		if ctx.IsDebug() {
+			ctx.Debugf("Here are the resources that are going to be uninstalled:")
+			resources := athensProfile.AllResources()
+			for _, res := range resources {
+				ctx.Debugf("%s %s/%s", res.Type(), res.Namespace().Name(), res.Name())
+				ctx.Debugf("%s", res)
+			}
+		}
+		ctx.Infof("Uninstalling %s", athensProfile)
+		if err := athensProfile.Uninstall(
+			ctx,
+			cl,
+			kube.ErrorStrategyContinue,
+		); err != nil {
+			return err
+		}
+		ctx.Infof("Done")
+
+		return nil
+	}
+	return ret
+}
