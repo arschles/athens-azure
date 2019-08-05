fcee5ea976ad832d5afb2ebf944a88ad96378ac1
Aaron Schlesinger <aaron@ecomaz.net>
2019-08-05 15:35:12 -0700

    more stuff :smile:

--- aci.sh
+++ aci.sh
@@ -9,7 +9,7 @@ do
     -g "${AZURE_ATHENS_RESOURCE_GROUP}" \
     -n "${AZURE_ATHENS_CONTAINER_NAME}-${LOCATION}" \
     --image gomods/athens:${IMAGE_TAG} \
-    -e "ATHENS_STORAGE_TYPE=mongo" "ATHENS_MONGO_STORAGE_URL=${AZURE_ATHENS_MONGO_URL}" \
+    -e "ATHENS_STORAGE_TYPE=mongo" "ATHENS_MONGO_STORAGE_URL=${AZURE_ATHENS_MONGO_URL}" "ATHENS_GO_GET_WORKERS=5" "GO_ENV=development" \
     --ip-address=Public \
     --dns-name="${AZURE_ATHENS_DNS_NAME}-${LOCATION}" \
     --ports="3000" \
