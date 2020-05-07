54dbe23fda26bf26273413d7de93752966587bdf
Aaron Schlesinger <aaron@ecomaz.net>
2020-05-07 15:14:34 -0700

    adding storage script
    and updating names in the terraform.tf

--- tf/terraform.tf
+++ tf/terraform.tf
@@ -1,12 +1,12 @@
 terraform {
   backend "azurerm" {
-    storage_account_name  = "athenstfstate"
-    container_name        = "athenstfstate"
-    key                   = "terraform.tfstate"    
+    storage_account_name  = "athensci"
+    container_name        = "tstate"
+    key                   = "terraform.tfstate" 
   }
 }
 
-resource "azurerm_resource_group" "athens-tfstate" {
+resource "azurerm_resource_group" "athens-state-secure" {
   name     = "athens-tfstate"
   location = "eastus"
 }
